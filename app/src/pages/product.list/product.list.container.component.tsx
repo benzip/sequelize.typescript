af3e1059d88b1614914650d339ff8cd96c96b26a
Nattapol <nattapol.sittikijyothin@gmail.com>
2020-03-19 23:37:14 +0700

    fix bug pagination

--- app/src/pages/product.list/product.list.container.component.tsx
+++ app/src/pages/product.list/product.list.container.component.tsx
@@ -3,91 +3,41 @@ import ProductListTable from "./product.list.table.component";
 import ListContentLayout from "../../layout/list.content.layout";
 import { useQuery } from "@apollo/react-hooks";
 import { Input } from "antd";
-import gql from "graphql-tag";
 const { Search } = Input;
-
-interface Product {
-  Id: number;
-  ProductName: string;
-  SearchName: string;
-}
-
-interface ProductQueryViewModel {
-  Data: Product[];
-  TotalCount: number;
-}
-
-interface QueryData {
-  products: ProductQueryViewModel;
-}
-
-const query = gql`
-  query Product($pageNum: Int!, $searchText: String!, $withTotalCount: Boolean!) {
-    products(pageNum: $pageNum, searchText: $searchText, withTotalCount: $withTotalCount) {
-      Data {
-        Id
-        ProductName
-        SearchName
-      }
-      TotalCount
-    }
-  }
-`;
 
 const ProductListContainerComponent = () => {
   const [state, setSate] = React.useState({
     searchText: "",
     withTotalCount: true,
-    currentPage: 1
-  });
-
-  const { data, loading, fetchMore } = useQuery<QueryData>(query, {
-    variables: { pageNum: 1, searchText: state.searchText, withTotalCount: state.withTotalCount },
-    notifyOnNetworkStatusChange: true
+    loading: true
   });
 
   const onSearch = (searchText: string) => {
     setSate({
       withTotalCount: true,
       searchText: searchText,
-      currentPage: 1
+      loading: state.searchText != searchText
     });
   };
 
-  const handlePageChange = (pageNum: any) => {
-    fetchMore({
-      variables: {
-        pageNum: pageNum,
-        searchText: state.searchText,
-        withTotalCount: false
-      },
-      updateQuery: (prev, { fetchMoreResult }) => {
-        if (!fetchMoreResult) return prev;
-        let ret = Object.assign({}, prev, {
-          products: {
-            Data: [...prev?.products.Data, ...fetchMoreResult?.products.Data],
-            TotalCount: prev.products.TotalCount,
-            __typename: "ProductQueryViewModel"
-          }
-        });
-        return ret;
-      }
+  const handleLoadComplete = () => {
+    setSate({
+      ...state,
+      loading: false
+    });
+  };
+
+  const handleLoadind = () => {
+    setSate({
+      ...state,
+      loading: true
     });
   };
 
   return (
     <ListContentLayout
-      searchTextBox={<Search onSearch={value => onSearch(value)} placeholder="input search text" style={{ width: "100%" }} loading={loading} />}
-      listContent={
-        <ProductListTable
-          searchText={state.searchText}
-          data={data?.products.Data}
-          totalCount={data?.products.TotalCount}
-          loading={loading}
-          currentPage={state.currentPage}
-          handlePageChange={(pageNum: number) => handlePageChange(pageNum)}
-        />
-      }
+      searchTextBox={<Search onSearch={value => onSearch(value)} placeholder="input search text" style={{ width: "100%" }} loading={state.loading} />}
+      listContent={<ProductListTable searchText={state.searchText} withTotalCount={state.withTotalCount} handleLoadComplete={handleLoadComplete} handleLoadind={handleLoadind} />}
     ></ListContentLayout>
   );
 };
--- app/src/pages/product.list/product.list.table.component.tsx
+++ app/src/pages/product.list/product.list.table.component.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Table } from "antd";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
 const columns = [
   {
@@ -15,32 +17,81 @@ const columns = [
   }
 ];
 
+interface Product {
+  Id: number;
+  ProductName: string;
+  SearchName: string;
+}
+
+interface ProductQueryViewModel {
+  Data: Product[];
+  TotalCount: number;
+}
+
+interface QueryData {
+  products: ProductQueryViewModel;
+}
+
+const query = gql`
+  query Product($pageNum: Int!, $searchText: String!, $withTotalCount: Boolean!) {
+    products(pageNum: $pageNum, searchText: $searchText, withTotalCount: $withTotalCount) {
+      Data {
+        Id
+        ProductName
+        SearchName
+      }
+      TotalCount
+    }
+  }
+`;
+
 const ProductListTableComponent = (props: any) => {
+  const { data, loading, fetchMore } = useQuery<QueryData>(query, {
+    variables: { pageNum: 1, searchText: props.searchText, withTotalCount: props.withTotalCount },
+    notifyOnNetworkStatusChange: true,
+    onCompleted: data => {
+      props.handleLoadComplete();
+    }
+  });
+
   const handleTableChange = (pagination: any, filters: any, sorter: any) => {
-    // const pager = { ...this.state.pagination };
-    // pager.current = pagination.current;
-    // this.setState({
-    //   pagination: pager
-    // });
-    // this.fetch({
-    //   results: pagination.pageSize,
-    //   page: pagination.current,
-    //   sortField: sorter.field,
-    //   sortOrder: sorter.order,
-    //   ...filters
-    // });
-    props.handlePageChange(pagination.current);
+    props.handleLoadind();
+    fetchMore({
+      variables: {
+        pageNum: pagination.current,
+        searchText: props.searchText,
+        withTotalCount: false
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) {
+          props.handleLoadComplete();
+          return prev;
+        }
+        let ret = Object.assign({}, prev, {
+          products: {
+            Data: [...fetchMoreResult?.products.Data],
+            TotalCount: prev.products.TotalCount,
+            __typename: "ProductQueryViewModel"
+          }
+        });
+
+        props.handleLoadComplete();
+        return ret;
+      }
+    });
   };
 
   return (
     <Table
       scroll={{ y: "67vh" }}
+      style={{ minHeight: "67vh" }}
       columns={columns}
-      dataSource={props.data}
+      dataSource={data?.products.Data}
       rowKey="Id"
-      loading={props.loading}
+      loading={loading}
       pagination={{
-        total: props.totalCount,
+        defaultCurrent: 1,
+        total: data?.products.TotalCount,
         pageSize: 20
       }}
       onChange={handleTableChange}
